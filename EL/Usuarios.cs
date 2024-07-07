f833ee25a95e5bdfa0bc6d1d795e21f3d52f7f9a
MiguelHD24 <m_angelmendezlopez@hotmail.es>
2024-07-07 11:12:29 -0600

    Actualizar EL

---
+++ EL/Usuarios.cs
@@ -0,0 +1,36 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace EL
+{
+	[Table("Usuarios")]
+	public class Usuarios
+	 {
+		[Key]
+		public int IdUsuario { get; set; }
+		[MaxLength(200)]
+		[Required]
+		public string NombreCompleto { get; set; }
+		[MaxLength(200)]
+		[Required]
+		public string Correo { get; set; }
+		[MaxLength(50)]
+		[Required]
+		public string NombreUsuario { get; set; }
+		public byte[]? Contrasena { get; set; }
+		[Required]
+		public bool Bloqueado { get; set; }
+		[Required]
+		public short IntentosFallidos { get; set; }
+		[Required]
+		public int IdRol { get; set; }
+		[Required]
+		public bool Activo { get; set; }
+		[Required]
+		public int IdUsuarioRegistra { get; set; }
+		[Required]
+		public DateTime FechaRegistro { get; set; }
+		public int? IdUsuarioActualiza { get; set; }
+		public DateTime? FechaActualizacion { get; set; }
+	 }
+}
