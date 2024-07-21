c5da3e5873c81e74a9576b4606e6e7c03497a117
ivan-iakoev <pablovichivan@gmail.com>
2024-07-21 09:51:05 -0600

    Añadiendo clases DAL RolFormulario y DAL RolPermiso

---
+++ DAL/DAL_RolFormulario.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public static class DAL_RoFormulario
+    {
+        public static RolFormulario Insert(RolFormulario Entidad)
+        {
+            using (BDInformaticSecuriy bd = new BDInformaticSecuriy())
+            {
+                Entidad.Activo = true;
+                Entidad.FechaRegistro = DateTime.Now;
+                bd.RolFormulario.Add(Entidad);
+                bd.SaveChanges();
+                return Entidad;
+            }
+        }
+        public static bool Update(RolFormulario Entidad)
+        {
+            using (BDInformaticSecuriy bd = new BDInformaticSecuriy())
+            {
+                var Registro = bd.RolFormulario.Find(Entidad.IdRolFormulario);
+                Registro.NombreRol = Entidad.NombreRol;
+                Registro.IdUsuarioActualiza = Entidad.IdUsuarioActualiza;
+                Registro.FechaActualizacion = Entidad.FechaActualizacion;
+                return bd.SaveChanges() > 0;
+            }
+        }
+        public static bool Anular(RolFormulario Entidad)
+        {
+            using (BDInformaticSecuriy bd = new BDInformaticSecuriy())
+            {
+                var Registro = bd.RolFormulario.Find(Entidad.IdRolFormulario);
+                Registro.Activo = Entidad.Activo;
+                Registro.IdUsuarioActualiza = Entidad.IdUsuarioActualiza;
+                Registro.FechaActualizacion = Entidad.FechaActualizacion;
+                return bd.SaveChanges() > 0;
+            }
+        }
+        public static bool Existe(RolFormulario Entidad)
+        {
+            using (BDInformaticSecuriy bd = new BDInformaticSecuriy())
+            {
+                return bd.RolFormulario.Where(a => a.IdRolFormulario == Entidad.IdRolFormulario).Count() > 0;
+            }
+        }
+        public static RolFormulario Registro(RolFormulario Entidad)
+        {
+            using (BDInformaticSecuriy bd = new BDInformaticSecuriy())
+            {
+                return bd.RolFormulario.Where(a => a.IdRol == Entidad.IdRolFormulario).SingleOrDefault();
+            }
+        }
+        public static List<RolFormulario> Lista(bool Activo = true)
+        {
+            using (BDInformaticSecuriy bd = new BDInformaticSecuriy())
+            {
+                return bd.RolFormulario.Where(a => a.Activo == Activo).ToList();
+            }
+        }
+    }
+}
---
+++ DAL/DAL_RolPermiso.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public static class DAL_RoPermiso
+    {
+        public static RolPermiso Insert(RolPermiso Entidad)
+        {
+            using (BDInformaticSecuriy bd = new BDInformaticSecuriy())
+            {
+                Entidad.Activo = true;
+                Entidad.FechaRegistro = DateTime.Now;
+                bd.RolPermiso.Add(Entidad);
+                bd.SaveChanges();
+                return Entidad;
+            }
+        }
+        public static bool Update(RolPermiso Entidad)
+        {
+            using (BDInformaticSecuriy bd = new BDInformaticSecuriy())
+            {
+                var Registro = bd.RolPermiso.Find(Entidad.IdRolPermiso);
+                Registro.NombreRol = Entidad.NombreRol;
+                Registro.IdUsuarioActualiza = Entidad.IdUsuarioActualiza;
+                Registro.FechaActualizacion = Entidad.FechaActualizacion;
+                return bd.SaveChanges() > 0;
+            }
+        }
+        public static bool Anular(RolPermiso Entidad)
+        {
+            using (BDInformaticSecuriy bd = new BDInformaticSecuriy())
+            {
+                var Registro = bd.RolPermiso.Find(Entidad.IdRolPermiso);
+                Registro.Activo = Entidad.Activo;
+                Registro.IdUsuarioActualiza = Entidad.IdUsuarioActualiza;
+                Registro.FechaActualizacion = Entidad.FechaActualizacion;
+                return bd.SaveChanges() > 0;
+            }
+        }
+        public static bool Existe(RolPermiso Entidad)
+        {
+            using (BDInformaticSecuriy bd = new BDInformaticSecuriy())
+            {
+                return bd.RolPermiso.Where(a => a.IdRolPermiso == Entidad.IdRolPermiso).Count() > 0;
+            }
+        }
+        public static RolPermiso Registro(RolPermiso Entidad)
+        {
+            using (BDInformaticSecuriy bd = new BDInformaticSecuriy())
+            {
+                return bd.RolPermiso.Where(a => a.IdRolPermiso == Entidad.IdRolFormulario).SingleOrDefault();
+            }
+        }
+        public static List<RolPermiso> Lista(bool Activo = true)
+        {
+            using (BDInformaticSecuriy bd = new BDInformaticSecuriy())
+            {
+                return bd.RolPermiso.Where(a => a.Activo == Activo).ToList();
+            }
+        }
+    }
+}
