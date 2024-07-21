77e5dab1e4a45075ee14960c0cf8588b5528ac47
mikemem44 <mikelmm1999@gmail.com>
2024-07-21 11:42:08 -0600

    Se agrego Formularios, Permisos y Usuario

---
+++ BL/BL_Usuario.cs
@@ -0,0 +1,53 @@
+﻿using DAL;
+using EL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public static class BL_Usuario
+    {
+        public static Usuarios Insert(Usuarios Entidad)
+        {
+            return DAL_Usuario.Insert(Entidad);
+        }
+
+        public static bool Update(Usuarios Entidad)
+        {
+            return DAL_Usuario.Update(Entidad);
+        }
+
+        public static bool Anular(Usuarios Entidad)
+        {
+            return DAL_Usuario.Anular(Entidad);
+        }
+
+        public static List<Usuarios> Lista(bool Activo = true)
+        {
+            return DAL_Usuario.Lista();
+        }
+
+        public static bool Existe(Usuarios Entidad)
+        {
+            return DAL_Usuario.Existe(Entidad);
+        }
+
+        public static int ObtenerIDUsuario(string username)
+        {
+            return DAL_Usuario.ObtenerIDUsuario(username);
+        }
+
+        public static bool ValidarCredenciales(Usuarios Entidad)
+        {
+            return DAL_Usuario.ValidarCredenciales(Entidad);
+        }
+
+        public static byte[] Sha256(string input)
+        {
+            return DAL_Usuario.Sha256(input);
+        }
+    }
+}
