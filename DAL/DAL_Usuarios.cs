25417b08a73981da0bba7a200557f61f44df3655
MiguelHD24 <m_angelmendezlopez@hotmail.es>
2024-07-21 11:29:20 -0600

    Se agrego carpeta asset

---
+++ DAL/DAL_Usuarios.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EL;
+
+namespace DAL
+{
+    public static class DAL_Usuarios
+    {
+        public static bool ValidarUserName(string UserName)
+        {
+            using (BDInformaticSecuriy bd = new BDInformaticSecuriy())
+            {
+                return bd.Usuarios.Where(a => a.NombreUsuario == UserName).Count() > 0;
+            }
+        }
+        public static bool ValidarPassword(byte[] Password)
+        {
+            using (BDInformaticSecuriy bd = new BDInformaticSecuriy())
+            {
+                return bd.Usuarios.Where(a => a.Contrasena == Password).Count() > 0;
+
+            }
+        }
+        public static Usuarios Login(string UserName, byte[] Password)
+        {
+            using (BDInformaticSecuriy bd = new BDInformaticSecuriy())
+            {
+                return bd.Usuarios.Where(a => a.NombreUsuario == UserName && a.Contrasena == Password).SingleOrDefault();
+            }
+        }
+
+    }
+}
